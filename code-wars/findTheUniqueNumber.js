02b76f9eb1bd62189b4745fbf0398b935f68fc50
OdeliaFink <odelia.finkel@gmail.com>
2021-05-09 11:58:25 -0400

    Refactoring function

--- code-wars/findTheUniqueNumber.js
+++ code-wars/findTheUniqueNumber.js
@@ -1,1 +1,14 @@
-function findUniq(arr) {}
+function findUniq(arr) {
+  arr.reduce((tally, val) => {}, {});
+}
+
+console.log(findUniq([1, 1, 1, 2, 2, 1, 1]));
+
+// let unique = new Set(arr);
+// let secondArray = [...unique];
+// let count = 0;
+// for (let i = 0; i < 4; i++) {
+//   if (arr[i] == secondArray[0]) {
+//     count++;
+//   }
+// }
