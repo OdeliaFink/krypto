e0c99c7ba47f164b371794e2ef8727bb65ea7f84
OdeliaFink <odelia.finkel@gmail.com>
2021-06-28 19:34:05 -0400

    Completed

--- code-wars/imEverywhere.js
+++ code-wars/imEverywhere.js
@@ -1,46 +1,27 @@
 /*
 
-
+The word should not begin with the letter "I", for example Inspire.
 The number of vowels should not be greater than or equal to the number of consonants, for example East or Peace. ("y" is considered a consonant)
-The first letter should not be lowercase, for example road.
+The first target should not be lowercase, for example road.
 If the word does not meet the rules, we return "Invalid word".
 */
 
 const everywhere = function i(word) {
-  const vowels = ['a', 'e', 'o', 'u', 'i'];
-  const consonants = [
-    'b',
-    'c',
-    'd',
-    'f',
-    'g',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'p',
-    'q',
-    's',
-    't',
-    'v',
-    'x',
-    'z',
-    'h',
-    'r',
-    'w',
-    'y',
-  ];
-
-  if (word.charAt(0) === 'i' || word.charAt(0) === 'I') {
-    return null;
-  } else if (word.includes(vowels)) {
-  } else {
-    return word;
-  }
-
-  // console.log(word.includes(vowels));
+  const i = (word) => {
+    if (
+      word === '' ||
+      word[0].toLowerCase() === 'i' ||
+      word[0] === word[0].toLowerCase()
+    )
+      return 'Invalid word';
+    if (word.replace(/[aeiou]/gi, '').length * 2 <= word.length)
+      return 'Invalid word';
+    return `i${word}`;
+  };
 };
 
-console.log(everywhere('Island'));
+// console.log(everywhere('Island'));
+console.log(everywhere('Phone'));
+console.log(everywhere('Odelia'));
+console.log(everywhere('tax'));
 console.log(everywhere('odelia'));
