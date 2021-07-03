5cfa2344d5d00d643ac59bd665413f62b63dc62a
OdeliaFink <odelia.finkel@gmail.com>
2021-07-03 18:00:52 -0400

    7kyu

---
+++ code-wars/7kyu/sortVowels.js
@@ -0,0 +1,13 @@
+/* 
+List all the Vowels on the right side of |
+List every character except Vowels on the left side of |
+Case doesn't matter
+Each line is seperated with \n
+Invalid input ( undefined / null / integer ) should return an empty string
+*/
+
+function sortVowels(s) {
+  //code
+}
+
+sortVowels('Codewars'); // 'C|\n|o\nd|\n|e\nw|\n|a\nr|\ns|');
