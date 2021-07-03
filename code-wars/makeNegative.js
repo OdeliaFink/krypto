ab1fe2ad82a62011c5cbcc8da920dce870cc9990
OdeliaFink <odelia.finkel@gmail.com>
2021-07-03 11:03:27 -0400

    Mean of Array 8kyu

---
+++ code-wars/makeNegative.js
@@ -0,0 +1,5 @@
+function makeNegative(num) {
+  return num * -1;
+}
+
+console.log(makeNegative(42));
--- code-wars/meanOfArray.js
+++ code-wars/meanOfArray.js
@@ -2,12 +2,7 @@ function getAverage(marks) {
   //TODO : calculate the downward rounded average of the marks array
 
   const sum = marks.reduce((a, b) => a + b, 0);
-  return sum / marks.length;
-
-  // let total = 0;
-  // // for (let i = 0; i < marks.length; i++) {
-  // console.log(marks.length);
-  // // }
+  return Math.floor(sum / marks.length);
 }
 
 console.log(getAverage([2, 2, 2, 2])); //2;
