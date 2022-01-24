e721d78f6a0b664c74313c5f3ee25d3421c1aa74
OdeliaFink <odelia.finkel@gmail.com>
2022-01-24 15:37:09 -0500

    connecting wallet

--- Desktop/Web 3.0/client/src/components/Welcome.jsx
+++ Desktop/Web 3.0/client/src/components/Welcome.jsx
@@ -1,6 +1,8 @@
+import React, { useContext } from 'react';
 import { AiFillPlayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
+import { TransactionContext } from '../context/TransactionContext';
 
 import Loader from './Loader';
 
@@ -19,6 +21,9 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
+  const { value } = useContext(TransactionContext);
+  console.log(value);
+
   const connectWallet = () => {};
   const handleSubmit = () => {};
 
--- Desktop/Web 3.0/smart-contract/hardhat.config.js
+++ Desktop/Web 3.0/smart-contract/hardhat.config.js
@@ -1,10 +1,13 @@
+// 'e00a7f0dced484e75476ca17168b1ca546037b522df9402ad8a6d79fcf4f0b6d',
+//https://eth-ropsten.alchemyapi.io/v2/yGziFO13cYdZF7dEoCRMznJbUMZgflRG
+
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropster: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/yGziFO13cYdZF7dEoCRMznJbUMZgflRG',
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
       accounts: [
         'e00a7f0dced484e75476ca17168b1ca546037b522df9402ad8a6d79fcf4f0b6d',
       ],
