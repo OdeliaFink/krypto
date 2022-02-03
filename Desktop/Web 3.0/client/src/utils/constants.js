33562032b851a30c16cd49a182a95c4117f5f171
Odelia Finkelstein <73666565+OdeliaFink@users.noreply.github.com>
2022-02-03 15:30:50 -0500

    Delete Desktop/Web 3.0 directory

--- Desktop/Web 3.0/client/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-import abi from './Transactions.json';
-//everything we need to interact with our Ethereum smart contract
-export const contractABI = abi.abi;
-export const contractAddress = '0x2E93aB625589FBB378EDA8392A30d1b9f7b4fF92';
